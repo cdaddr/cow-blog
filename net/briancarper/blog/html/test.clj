33f71c5235f222bf94245b29e5cfd46dda1004d3
Brian Carper <brian@briancarper.net>
2009-10-22 23:08:21 -0700

    Starting over from scratch

--- net/briancarper/blog/html/test.clj
+++
@@ -1,8 +0,0 @@
-(ns net.briancarper.blog.html.test
-  (use (net.briancarper.blog html)))
-
-(in-ns 'net.briancarper.blog.html)
-
-(defmacro logged-in [& rest]
-  `(binding [*session* (ref {:username "foo"})]
-     ~@rest))
