bf3aa85fa72edef7fbba78480821738a294546e8
Brian Carper <brian@carper.ca>
2010-05-21 17:46:40 -0700

    restructure, use leiningen (everything b0rked)

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cow-blog "0.2.0-SNAPSHOT"
+  :description "Just another blog engine, with cows"
+  :url "http://github.com/briancarper/cow-blog"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [compojure "0.4.0-RC3"]])
