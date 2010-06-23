b3c35773e3193e07be0774601756e33a21e44fef
Brian Carper <brian@carper.ca>
2010-06-22 22:05:29 -0700

    Remove old admin code from config.clj

--- src/blog/config.clj
+++ src/blog/config.clj
@@ -8,15 +8,6 @@
 (def SITE-DESCRIPTION "Some guy's blog about Clojure.")
 
 (def PUBLIC-DIR "public")  ;;CS/JS/images live here.  Relative path.
-
-;; For now, only a single admin user can exist, and this is where the login details live.
-;; use blog.admin/generate-user to generate a new user and then put it here.
-;; Below:
-;;   username = foo
-;;   password = bar
-(def ADMIN-USER {:username "foo"
-                 :password "4bdec02a2dd5e6b6e28935bccf9bf4e7e5becce96b7845bee692768f4e4a810"})
-
 (def TIME-ZONE "Canada/Pacific")
 (def TIME-FORMAT "MMMM dd, yyyy @ h:mm a z")
 
